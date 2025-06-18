dca5e544dca40503a3ea8b9136e3a16295d2768a
dttrue <danieltorres@pursuit.org>
2025-06-17 21:18:00 -0400

    Same as before

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,15 +1,13 @@
 // app/admin/page.js
 
 import NewsletterDashboard from "@/components/dashboard/AdminDashboard";
-import GalleryDashboard from "@/components/dashboard/GalleryDashboard";
-import PruneButton from "@/archive/gallery/PruneButton";
+
 
 export default function AdminNewsletterPage() {
   return (
     <>
       <NewsletterDashboard />
-      <GalleryDashboard />
-      <PruneButton />
+      
     </>
   );
 }
